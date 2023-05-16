9b5bb8749fffc7ff78dc1c785f18553ed7bdd2e9
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-05-16 14:35:37 +0200

    njee

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let subject = [ "Python" , "C++","VB.Net","Javascript","Assembly"]
+console.log(subject)
+ let last  = subject[4]
+ console.log(last)
+
+ let middle = subject[2]
+ console.log(middle)
+
+
